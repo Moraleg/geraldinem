df2bd4b7eb4b60100a01d1ca46ad07785692ed8f
Geraldine Morales <geraldine.morales02@gmail.com>
2017-06-04 21:30:02 -0700

    added some repsonsive styling

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('section.hidden').fadeIn(1000).removeClass('hidden');
+});
+
+
+// slideTimer = setInterval(function() {
+//     $('.section-one-container').slideUp();
+// }, 2000);
