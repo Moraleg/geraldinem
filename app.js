daf6e89ac4bdb97e0408266b6a584e7d283e6164
Geraldine Morales <geraldine.morales02@gmail.com>
2017-06-04 21:58:43 -0700

    workingon responsive styling

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ $(document).ready(function () {
 });
 
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
 // slideTimer = setInterval(function() {
 //     $('.section-one-container').slideUp();
 // }, 2000);
