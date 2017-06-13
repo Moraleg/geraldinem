2f22c35f82001bf4222e9181db1ae4db76907d8c
Geraldine Morales <geraldine.morales02@gmail.com>
2017-06-13 15:45:54 -0400

    added more css styling

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     $('section.hidden').fadeIn(1000).removeClass('hidden');
 });
 
+$(document).ready(function () {
+    $('main.hidden').fadeIn(1000).removeClass('hidden');
+});
 
 // slideTimer = setInterval(function() {
 //     $('.section-one-container').slideUp();
