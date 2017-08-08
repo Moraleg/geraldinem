cca2688bc4fedbeba27cb7ca2de845e4403d4241
Geraldine Morales <geraldine.morales02@gmail.com>
2017-08-07 20:34:02 -0400

    added profile photo, transitions, and modified css

--- app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('section.hidden').fadeIn(1000).removeClass('hidden');
-});
-
-$(document).ready(function () {
-    $('main.hidden').fadeIn(3500).removeClass('hidden');
-});
---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+   
+   $('.profile-box').animate({'margin-top': '0'}, 1000);
+   $('header.hidden').fadeIn(2000).removeClass('hidden');
+   $('section.hidden').fadeIn(2000).removeClass('hidden');
+   $('main.hidden').fadeIn(2000).removeClass('hidden');
+
+}); //Window Onload
